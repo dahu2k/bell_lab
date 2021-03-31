d174a6c98a6a2e10ff353e7d674af834a78a3b2c
Diego Hernandez <diegohernandez@sandiego.edu>
2021-03-31 12:25:37 -0700

    implemented plot_ddg_vs_res and ddg_x_res_y

--- ddgsorter.py
+++ ddgsorter.py
@@ -7,6 +7,8 @@ Author:
 Diego Hernandez
 """
 
+import matplotlib.pyplot as ppl
+
 def txt_to_csv(filename_in, filename_out):
     """
         Formats .txt file of DDG Data to .csv file.
@@ -35,9 +37,54 @@ def txt_to_csv(filename_in, filename_out):
 
     return None
 
+def ddg_x_res_y(filename):
+    """
+    Makes a list from ddg data in .csv file and returns residue # as x value and ddg
+    as y value.
+    Parameters:
+    filename (type: string): name of .csv file with ddg data
+    Returns:
+    res_x (type: list with int): list of residue numbers
+    ddg_y (type: list with int): list of ddg values
+    """
+
+
+    ddg_data_file = open(filename, 'r', encoding="utf-8")
+
+    ddg_data_file.readline()
+    res_x = []
+    ddg_y = []
+    for line in ddg_data_file:
+        val = line.split(",")
+        res_x.append(float(val[2]))
+        ddg_y.append(float(val[4]))
+
+    return (res_x, ddg_y) # Returns a tuple of residue numbers and ddg values
+
+def plot_ddg_vs_res(filename):
+    """
+    Plots ddg data on the y axis according to its residue on the x axis.
+    Parameters:
+    filename (type: string): name of .csv file with ddg data
+    Returns:
+    None
+    """
+
+    x_val, y_val = ddg_x_res_y(filename)
+    ppl.scatter(x_val, y_val)
+    ppl.show()
+    
+
+    return None
+
 def main():
-    filename_in = input('filename_in: ')
-    filename_out = input('filename out: ')
-    txt_to_csv(filename_in, filename_out)
+    choice = int(input('what do you want to do?\n1) convert .txt of ddg data to .csv\n2) plot .ddg data from .csv\nChoose by inputing interger: '))
+    if choice == 1:
+        filename_in = input('filename_in: ')
+        filename_out = input('filename out: ')
+        txt_to_csv(filename_in, filename_out)
+    if choice == 2:
+        filename = input('filename: ')
+        plot_ddg_vs_res(filename)
 if __name__ == "__main__":
     main()
